4b16d5b59d66e805ba6a4db7aa716ee820341ebb
Carlos <lazc.0609@gmail.com>
2025-11-20 22:35:07 -0600

    Cambios en la db

--- back/config.py
+++ back/config.py
@@ -1,7 +1,10 @@
 import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(BASE_DIR, 'app.db')
+    if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
+        
     SQLALCHEMY_TRACK_MODIFICATIONS = False
